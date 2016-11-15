8f457ca41899af76c7eec530946aec5be19b580d
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-15 19:30:00 +0300

    kot

--- solar_model.py
+++ solar_model.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # license: GPLv3
-import math
+
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
@@ -18,8 +18,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        sina = math.fabs(((body.y - obj.y)/r))
-        cosa = math.fabs(((body.x - obj.x)/r))
+        sina = (((body.y - obj.y)/r))
+        cosa = (((body.x - obj.x)/r))
         print(sina)
         F = gravitational_constant*(body.m*obj.m)/(r**2) 
         body.Fx = F * cosa
@@ -55,4 +55,4 @@ def recalculate_space_objects_positions(space_objects, dt):
 
 
 if __name__ == "__main__":
-    print("This module is not for direct call!")   
+    print("This module is not for direct call!")
