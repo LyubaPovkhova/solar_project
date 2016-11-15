5e407dc35dfb985e38a85b3a75e810af1103646c
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-15 18:55:04 +0300

    owl

--- solar_model.py
+++ solar_model.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # license: GPLv3
+import math
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
@@ -18,9 +19,13 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx=F*cosa
-        body.Fy=F*sina
+         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        sina = math.fabs(((body.y - obj.y)/r))
+        cosa = math.fabs(((body.x - obj.x)/r))
+        print(sina)
+        F = gravitational_constant*(body.m*obj.m)/(r**2)
+        body.Fx = F * cosa
+        body.Fy = F * sina 
 
 
 def move_space_object(body, dt):
